d3c75b2834eeb3a6a394474f1b528777c8772798
Shipon Hossen Raju <msshipon234@gmail.com>
2024-04-20 21:39:56 +0600

    initial setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,9 @@
+// Home page
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col gap-7 items-center justify-center">
+      <h1 className="text-3xl font-bold underline"> Assalamualaikum </h1>
+      <p className="text-lg"> Welcome to my website </p>
+    </div>
+  );
+}
